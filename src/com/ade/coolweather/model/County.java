b10ca365864b6ca05a67158621b255211cd5d758
Qishiyuan <Qishiyuan123@gmail.com>
2015-03-31 22:29:46 +0800

    第三次

---
+++ src/com/ade/coolweather/model/County.java
@@ -0,0 +1,40 @@
+package com.ade.coolweather.model;
+
+public class County {
+	private int id;
+	private String countyName;
+	private String countycode;
+	private int cityId;
+	public int getCityId() {
+		return cityId;
+	}
+	public void setCityId(int cityId) {
+		this.cityId = cityId;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCountyName() {
+		return countyName;
+	}
+	public void setCountyName(String countyName) {
+		this.countyName = countyName;
+	}
+	public String getCountycode() {
+		return countycode;
+	}
+	public void setCountycode(String countycode) {
+		this.countycode = countycode;
+	}
+	public int getProvinceId() {
+		return cityId;
+	}
+	public void setProvinceId(int cityId) {
+		this.cityId = cityId;
+	}
+	
+	
+}
