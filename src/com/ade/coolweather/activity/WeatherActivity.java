bc574a552a8fa87e689ec2868ffb28eb939b8828
Qishiyuan <Qishiyuan123@gmail.com>
2015-04-01 15:42:26 +0800

    增加后台更新

--- src/com/ade/coolweather/activity/WeatherActivity.java
+++ src/com/ade/coolweather/activity/WeatherActivity.java
@@ -14,6 +14,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.ade.coolweather.R;
+import com.ade.coolweather.service.AutoUpdateService;
 import com.ade.coolweather.util.HttpCallbackListener;
 import com.ade.coolweather.util.HttpUtil;
 import com.ade.coolweather.util.Utility;
@@ -159,5 +160,7 @@ public class WeatherActivity extends Activity implements OnClickListener {
 		currentDateText.setText(prefs.getString("current_date", ""));
 		weatherInfoLayout.setVisibility(View.VISIBLE);
 		cityNameText.setVisibility(View.VISIBLE);
+		Intent intent=new Intent(this, AutoUpdateService.class);
+		startService(intent);
 	}
 }
---
+++ src/com/ade/coolweather/receiver/AutoUpdateReceiver.java
@@ -0,0 +1,16 @@
+package com.ade.coolweather.receiver;
+
+import com.ade.coolweather.service.AutoUpdateService;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AutoUpdateReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Intent i=new Intent(context, AutoUpdateService.class);
+		context.startService(i);
+	}
+}
